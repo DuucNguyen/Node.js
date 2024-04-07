7ebe62660a04fd2d9c6b8542aa2ca67309d65884
DuucNguyen <ducnthe173064@fpt.edu.vn>
2024-04-07 21:35:51 +0700

    query param using GET method

--- src/server.js
+++ src/server.js
@@ -6,25 +6,32 @@ const path = require("path");
 const app = express();
 const port = 8081;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"))); //set path to use folder public
 
-// HTTP logger
-app.use(morgan("combined"));
+// HTTP logger log the path/port the app is listening
+// app.use(morgan("combined"));
 
 // Template engine setup
-app.engine(".hbs", engine({ extname: ".hbs" }));
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
-
+app.engine(".hbs", engine({ extname: ".hbs" })); //set shorthand for handlers file for app to recognize
+app.set("view engine", "hbs"); //set view as handlerbars (using handlebars as html or view part)
+app.set("views", path.join(__dirname, "resources/views")); //set path to views folder (find the corresponding view and use it as view)
 // console.log(path.join("Path: " + __dirname, "resources/views"));
 
 // Define routes
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home"); //render a html code (response) as view
 });
 
 app.get("/news", (req, res) => {
   res.render("news");
+});
+
+app.get("/search", (req, res) => {
+  var value = req.query.q;
+  if (value.includes("ode")) {
+    console.log("->Found");
+  }
+  res.render("search");
 });
 
 // Listen on port
