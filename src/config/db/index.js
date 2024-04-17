d0800e2308f03f80ef0e683d610834ba3ae85cec
DuucNguyen <ducnthe173064@fpt.edu.vn>
2024-04-17 15:57:14 +0700

    config timeout

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 async function connect() {
     try {
-        await mongoose.connect(process.env.DATABASE_URL);
+        await mongoose.connect(process.env.DATABASE_URL, {
+            serverSelectionTimeoutMS: 30000,
+        });
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Failed to connect to MongoDB:", error);
