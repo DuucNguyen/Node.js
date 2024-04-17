14dba0caa90f31dbdd740145c80a4698d500c61b
DuucNguyen <ducnthe173064@fpt.edu.vn>
2024-04-17 12:31:45 +0700

    rename paths

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const newsController = require("../app/controllers/newsController"); //already included
+const newsController = require("../app/controllers/NewsController"); //already included
 
 router.get("/:slug", newsController.show);
 router.get("/", newsController.index); //defaul url must be last
